3ad016f68e9e187e7d4aa51dbb1912fb3415714d
trevorwattles <twattles@byu.edu>
2025-03-10 17:03:31 -0600

    changed line of code that did not display correctly

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -8,7 +8,7 @@ export function Login({ userName, authState, onAuthChange }) {
   return (
     <main className='container-fluid bg text-center'>
       <div>
-        {authState !== AuthState.Unknown && <h1>Welcome to Simon</h1>}
+        {authState !== AuthState.Unknown && <h1>Welcome to Gigglr</h1>}
         {authState === AuthState.Authenticated && (
           <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
         )}
